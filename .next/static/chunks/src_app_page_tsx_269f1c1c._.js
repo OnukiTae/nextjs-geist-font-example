8af338f3f4da4ffe67acb154c3bc6a94454d8420
OnukiTae <412senio@gmail.com>
2025-08-03 22:53:12 +0900

    Add .next/static/chunks/src_app_page_tsx_269f1c1c._.js 1754229193367

---
+++ .next/static/chunks/src_app_page_tsx_269f1c1c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1837c7e1._.js",
+  "static/chunks/node_modules_cc8f66e3._.js"
+],
+    source: "dynamic"
+});
